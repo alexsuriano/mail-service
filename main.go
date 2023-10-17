c88dfab9f769baa75013287c9d303f3a2a2bc0d3
Alexandre Suriano <alexandre.suriano@gmail.com>
2023-10-17 14:53:12 -0300

    simple project to send email

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"gopkg.in/gomail.v2"
+)
+
+type ConfigMail struct {
+	host     string
+	port     int
+	user     string
+	password string
+}
 
 func main() {
-	fmt.Println("REFACTORING")
+	mail := ConfigMail{
+		host:     "example.smt.mail.com",
+		port:     587,
+		user:     "johndo@mail.com",
+		password: "123456",
+	}
+
+	msg := gomail.NewMessage()
+	msg.SetHeader("From", "johndo@mail.com")
+	msg.SetHeader("To", "otherguy@mail.com")
+	msg.SetHeader("Subject", "test message")
+	msg.SetBody("text/html", "<h1> Body test </h1>")
+
+	dialer := gomail.NewDialer(mail.host, mail.port, mail.user, mail.password)
+
+	err := dialer.DialAndSend(msg)
+	if err != nil {
+		log.Printf("Error sending the email: %v\n", err)
+	} else {
+		log.Println("Email successfully sent")
+	}
+
 }
